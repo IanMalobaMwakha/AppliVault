2dc1a6bb4c87a63db44b2dcdeede1d11a87b5b87
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-09-11 02:31:09 +0300

    modified:   applivault/job/models.py

--- applivault/job/models.py
+++ applivault/job/models.py
@@ -2,3 +2,16 @@ from django.db import models
 
 class Job(models.Model):
     job_name = models.CharField(max_length=125)
+
+    EXPERIENCE_LEVEL_CHOICES = (
+        ('Internship', 'Internship'),
+        ('Entry Level', 'Entry Level'),
+        ('Associate', 'Associate'),
+        ('Mid-Senior Level', 'Mid-Senior Level'),
+        ('Senior Level', 'Senior Level'),
+        ('Director', 'Director'),
+        ('Exercutive', 'Exercutive')
+    )
+    experience_level = models.CharField(max_length=100, choices=EXPERIENCE_LEVEL_CHOICES)
+
+    
