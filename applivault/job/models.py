000949de933946b5c8d1abb6f5e87ff8e638b060
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-09-14 18:21:04 +0300

    modified:   applivault/job/admin.py

--- applivault/job/models.py
+++ applivault/job/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+from multiselectfield import MultiSelectField
+from django.contrib.auth.models import User
+from datetime import date
+
+
+class Job(models.Model):
+    job_name = models.CharField(max_length=125)
+    job_poster = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    date_posted = models.DateField(auto_now_add=True)
+
+    EXPERIENCE_LEVEL_CHOICES = (
+        ('Internship', 'Internship'),
+        ('Entry Level', 'Entry Level'),
+        ('Associate', 'Associate'),
+        ('Mid-Senior Level', 'Mid-Senior Level'),
+        ('Senior Level', 'Senior Level'),
+        ('Director', 'Director'),
+        ('Executive', 'Executive')
+    )
+    experience_level = MultiSelectField(max_length=125, choices=EXPERIENCE_LEVEL_CHOICES)
+
+    def __str__(self):
+        return self.job_name
