10ce3152e9563b4bbafac38d94e3f9285fbccb0c
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-09-17 02:57:11 +0300

    modified:   applivault/db.sqlite3

--- applivault/job/models.py
+++ applivault/job/models.py
@@ -6,7 +6,8 @@ from django.contrib.auth.models import User
 class Job(models.Model):
     job_name = models.CharField(max_length=125)
     job_poster = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-
+    company_name = models.CharField(max_length=100, blank=True, null=True)
+    company_website_link = models.CharField(max_length=500, blank=True, null=True)
     EXPERIENCE_LEVEL_CHOICES = (
         ('Internship', 'Internship'),
         ('Entry Level', 'Entry Level'),
@@ -17,7 +18,6 @@ class Job(models.Model):
         ('Executive', 'Executive')
     )
     experience_level = MultiSelectField(max_length=125, choices=EXPERIENCE_LEVEL_CHOICES, blank=True)
-    job_details = models.TextField(null=True, blank=True)
 
     JOB_TYPE_CHOICES = (
         ('Remote', 'Remote'),
@@ -25,6 +25,8 @@ class Job(models.Model):
         ('Hybrid', 'Hybrid')
     )
     job_type = MultiSelectField(max_length=125, choices=JOB_TYPE_CHOICES, blank=True)
+    job_details = models.TextField(null=True, blank=True)
+    job_application_link = models.CharField(max_length=125, blank=True, null=True)
 
     def __str__(self):
         return self.job_name
