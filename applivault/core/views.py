d65a82cb2e9b99a57390d54ba6fde7fb5a047679
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-10-01 20:08:53 +0300

    pplivault/core/__pycache__/views.cpython-310.pyc modified:   applivault/core/views.py

--- applivault/core/views.py
+++ applivault/core/views.py
@@ -18,7 +18,15 @@ def search(request):
         query = request.GET.get('query')
         if query:
             jobs = Job.objects.filter(
-                Q(job_name__icontains=query) | Q(job_details__icontains=query)
+                Q(job_name__icontains=query) | 
+                Q(job_poster__username__icontains=query) |
+                Q(company_name__icontains=query) |
+                Q(company_website_link__icontains=query) |
+                Q(job_alternative_location__icontains=query) |
+                Q(experience_level__icontains=query) |
+                Q(job_type__icontains=query) |
+                Q(job_details__icontains=query)|
+                Q(job_salary__icontains=query)
                 )
             return render(request, "core/search_jobs.html", {
                 'jobs': jobs,
