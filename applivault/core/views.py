d820481c0cca0d1677ef8b5fde0bd3e0c250eec3
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-10-01 19:42:18 +0300

    modified:   applivault/core/views.py

--- applivault/core/views.py
+++ applivault/core/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 from job.models import Job
 
+from django.db.models import Q
+
+
 def index(request):
     jobs = Job.objects.all()
 
@@ -14,7 +17,9 @@ def search(request):
     if request.method == 'GET':
         query = request.GET.get('query')
         if query:
-            jobs = Job.objects.filter(job_name__icontains=query)
+            jobs = Job.objects.filter(
+                Q(job_name__icontains=query) | Q(job_details__icontains=query)
+                )
             return render(request, "core/search_jobs.html", {
                 'jobs': jobs,
                 })
