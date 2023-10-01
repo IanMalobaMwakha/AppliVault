e283c28a24f86e4bd71d0aa0a5c622e7a2b6b98c
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-10-01 19:28:21 +0300

    search functionality

--- applivault/core/views.py
+++ applivault/core/views.py
@@ -10,7 +10,17 @@ def index(request):
     })
 
 def search(request):
+
+    if request.method == 'GET':
+        query = request.GET.get('query')
+        if query:
+            jobs = Job.objects.filter(job_name__icontains=query)
+            return render(request, "core/search_jobs.html", {
+                'jobs': jobs,
+                })
+
     jobs = Job.objects.all()
     return render(request, "core/search_jobs.html", {
         'jobs': jobs,
     })
+
