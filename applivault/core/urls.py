c1c403dcb27cae73f95f1984e50778843c453ba2
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-10-03 23:05:34 +0300

    modified:   applivault/core/templates/core/job_details.html

--- applivault/core/urls.py
+++ applivault/core/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.search, name="search"),
+    path('job-details/', views.job_details, name='job_details')
 ]
