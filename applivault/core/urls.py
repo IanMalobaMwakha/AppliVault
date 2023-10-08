f837674140c0dd54ac43ccc55e2cd7d66c790a9f
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-10-08 23:34:58 +0300

    modified:   applivault/core/__pycache__/urls.cpython-310.pyc

--- applivault/core/urls.py
+++ applivault/core/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.search, name="search"),
-    path('job-details/<int:pk>/', views.job_details, name='job_details')
+    path('job-details/<int:pk>/', views.job_details, name='job_details'),
+    path('saved-jobs/', views.saved_jobs, name='saved_jobs'),
 ]
