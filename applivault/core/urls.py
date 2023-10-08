7e2eba3bfda42ad2212a5efcbcf8a3b10a1223b2
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-10-08 23:55:12 +0300

    modified:   applivault/core/urls.py

--- applivault/core/urls.py
+++ applivault/core/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('job-details/<int:pk>/', views.job_details, name='job_details'),
     path('saved-jobs/', views.saved_jobs, name='saved_jobs'),
     path('applied-jobs/', views.applied_jobs, name='applied_jobs'),
+    path('post-a-job/', views.post_job, name='post_job'),
+    path('my-posted-jobs/', views.my_posted_jobs, name='my_posted_jobs'),
+
+
 ]
