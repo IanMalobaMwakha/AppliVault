3bc175d138d54523c4e658ab4dae549fd9e0e482
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-10-13 21:44:26 +0300

    modified:   applivault/userauth/views.py

--- applivault/userauth/views.py
+++ applivault/userauth/views.py
@@ -14,3 +14,6 @@ def register_request(request):
 		messages.error(request, "Unsuccessful registration. Invalid information.")
 	form = NewUserForm()
 	return render (request=request, template_name="main/register.html", context={"register_form":form})
+
+
+# https://ordinarycoders.com/blog/article/django-user-register-login-logout
